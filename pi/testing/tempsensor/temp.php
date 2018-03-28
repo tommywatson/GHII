5e615b4363dd64c45deb63cf4737f54d21631b26
tommy <coder@tommywatson.com>
2018-03-28 11:08:16 -0500

    Limit to 48hrs

--- pi/testing/tempsensor/temp.php
+++ pi/testing/tempsensor/temp.php
@@ -1,4 +1,4 @@
 <?php
-    system("echo \"".$_POST["date"].",".$_POST["temp"].",".$_POST["humidity"]."\" >> temp.csv");
+    system("echo \"".$_POST["date"].",".$_POST["temp"].",".$_POST["humidity"]."\" >> temp.all.csv");
+    system("tail -n2880 temp.all.csv > temp.csv");
 ?>
-
